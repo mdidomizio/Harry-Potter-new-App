994571a8bdf6518de88e46b4f753f770b3aacf50
mdidomizio <miriam.didomizio@googlemail.com>
2023-09-16 18:18:06 +0200

    created a topAppBar for the title

--- app/src/main/java/com/example/harrypotterappnew/HarryPotterScreen.kt
+++ app/src/main/java/com/example/harrypotterappnew/HarryPotterScreen.kt
@@ -10,11 +10,17 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -23,6 +29,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -32,6 +39,16 @@ fun HarryPotterScreen(viewModel: HarryPotterViewModel) {
     LaunchedEffect(Unit) {
         viewModel.fetchHarryPotterApi()
     }
+    /*TopAppBar(
+        title = { Text(
+            text = "Harry Potter World",
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+            )
+        },
+        colors = TopAppBarDefaults.centerAligned(),
+    )*/
+
             LazyColumn() {
                 if(data.isEmpty()) {
                     item {
@@ -90,5 +107,16 @@ fun HPImageCard(data: HarryPotterData){
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyUI() {
+    CenterAlignedTopAppBar(
+        title = { Text(text = "SemicolonSpace") },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = Color.Black
+        )
+    )
 }
 
--- app/src/main/java/com/example/harrypotterappnew/MainActivity.kt
+++ app/src/main/java/com/example/harrypotterappnew/MainActivity.kt
@@ -18,6 +18,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            /*YourProjectNameTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        MyUI()
+                    }*/
+            MyUI()
             HarryPotterScreen(viewModel)
         }
     }
