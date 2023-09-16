4c63758bf159085315f8f72b451b1fb30909446f
mdidomizio <miriam.didomizio@googlemail.com>
2023-09-16 15:28:43 +0200

    added viewmodel

---
+++ app/src/main/java/com/example/harrypotterappnew/HarryPotterViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.harrypotterappnew
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import java.lang.Exception
+
+class HarryPotterViewModel : ViewModel {
+    private val repository = HarryPotterRepository()
+    private val _harryPotterData = MutableLiveData<List<HarryPotterData>>()
+    val harryPotterData: LiveData<List<HarryPotterData>> = _harryPotterData
+
+    fun fetchHarryPotterApi(){
+        viewModelScope.launch {
+            try {
+                val data = repository.getHarryPotterData()
+                _harryPotterData.value = data
+            } catch (e: Exception) {
+                //handle error
+            }
+        }
+    }
+}
