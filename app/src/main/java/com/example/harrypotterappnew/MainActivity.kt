bbd25dadb673c18c663d1fe0302a82639d1faee1
mdidomizio <miriam.didomizio@googlemail.com>
2024-01-14 21:37:23 +0100

    added the zoom out of splash screen once the app is ready

--- app/src/main/java/com/example/harrypotterappnew/MainActivity.kt
+++ app/src/main/java/com/example/harrypotterappnew/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.harrypotterappnew
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -10,6 +13,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
@@ -19,6 +23,31 @@ class MainActivity : ComponentActivity() {
         installSplashScreen().apply{
             setKeepOnScreenCondition{
                 !viewModel.isReady.value
+            }
+            /*is setting the zoom put fo the splashScreen*/
+            setOnExitAnimationListener{ screen ->
+                val zoomX = ObjectAnimator.ofFloat(
+                    screen.iconView,
+                    View.SCALE_X,
+                    0.4f,
+                    0.0f
+                )
+                zoomX.interpolator = OvershootInterpolator()
+                zoomX.duration = 500L
+                zoomX.doOnEnd { screen.remove() }
+
+                val zoomY = ObjectAnimator.ofFloat(
+                    screen.iconView,
+                    View.SCALE_Y,
+                    0.4f,
+                    0.0f
+                )
+                zoomY.interpolator = OvershootInterpolator()
+                zoomY.duration = 500L
+                zoomY.doOnEnd { screen.remove() }
+
+                zoomX.start()
+                zoomY.start()
             }
         }
 
